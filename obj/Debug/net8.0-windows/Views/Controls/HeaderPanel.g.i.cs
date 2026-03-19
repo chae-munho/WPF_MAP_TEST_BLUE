20763e2a7220402505f25201b7c22580bf8ae124
chae-munho <ansgh4300@naver.com>
2026-03-19 11:54:06 +0900

    가은역, 구량리역 패널 위치 다시 변경

--- obj/Debug/net8.0-windows/Views/Controls/HeaderPanel.g.i.cs
+++ obj/Debug/net8.0-windows/Views/Controls/HeaderPanel.g.i.cs
@@ -53,7 +53,7 @@ namespace Map.Views.Controls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Map;V1.0.0.0;component/views/controls/headerpanel.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Map;component/views/controls/headerpanel.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Controls\HeaderPanel.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
